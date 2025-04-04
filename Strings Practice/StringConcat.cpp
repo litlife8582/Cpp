e9a9ff2e520289f25a91e01e539f93864cd4c5de
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2025-04-04 22:13:32 +0530

    done

---
+++ Strings Practice/StringConcat.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string a,b;
+    getline(cin,a);
+    getline(cin,b);
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<a+" "+b<<endl;
+    return 0;
+}
